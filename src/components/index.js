dff0b3b8e18ae80b15ca75b31ba5de86f93b5d0f
Felipe Sakuma <fycorsak@gmail.com>
2022-02-10 21:05:58 -0300

    chore: remove useless import in components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,14 +3,5 @@ import Button from './Button.jsx';
 import { SiteLogo } from './SiteLogo.js';
 import { StyledLink } from './StyledLink.js';
 import { toastError, toastSuccess } from './toasts.jsx';
-import { Background } from './Background.js';
-export {
-  Background,
-  Form,
-  Input,
-  Button,
-  SiteLogo,
-  StyledLink,
-  toastError,
-  toastSuccess,
-};
+
+export { Form, Input, Button, SiteLogo, StyledLink, toastError, toastSuccess };
