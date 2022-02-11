5b9826d905426bc059590fee08640be8bad2f826
Felipe Sakuma <fycorsak@gmail.com>
2022-02-10 21:03:45 -0300

    refactor: add background to whole site

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,21 @@ import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from '../contexts/AuthContext.js';
 import { Cart, Home, Product, Success, SignIn, SignUp } from '../pages';
 import '../styles/reset.css';
+import { Background } from './Background.js';
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/product' element={<Product />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/success' element={<Success />} />
-          <Route path='/signIn' element={<SignIn />} />
-          <Route path='/signUp' element={<SignUp />} />
-        </Routes>
+        <Background>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/product' element={<Product />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/success' element={<Success />} />
+            <Route path='/signIn' element={<SignIn />} />
+            <Route path='/signUp' element={<SignUp />} />
+          </Routes>
+        </Background>
         <ToastContainer />
       </BrowserRouter>
     </AuthProvider>
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,14 @@ import Button from './Button.jsx';
 import { SiteLogo } from './SiteLogo.js';
 import { StyledLink } from './StyledLink.js';
 import { toastError, toastSuccess } from './toasts.jsx';
-
-export { Form, Input, Button, SiteLogo, StyledLink, toastError, toastSuccess };
+import { Background } from './Background.js';
+export {
+  Background,
+  Form,
+  Input,
+  Button,
+  SiteLogo,
+  StyledLink,
+  toastError,
+  toastSuccess,
+};
