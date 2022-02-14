c9d878c6f0f87196d4fa2a848c61e1d538aaedcf
Felipe Sakuma <fycorsak@gmail.com>
2022-02-14 18:16:31 -0300

    fix: signin signup background color

--- src/pages/SignUp/style.js
+++ src/pages/SignUp/style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  background-color: #303030;
   display: flex;
   flex-direction: column;
   padding: 30px;
